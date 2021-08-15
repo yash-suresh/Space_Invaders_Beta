bce98d8fc3ceca71fffb1a1469df06a116e133ff
Yash Suresh <psyys5@nottingham.ac.uk>
2021-08-15 14:26:59 +0100

    moving player and enemy

---
+++ main.py
@@ -0,0 +1,97 @@
+import pygame
+
+# initialise the pygame library and functions.
+pygame.init()
+
+pygame.display.set_caption("Space Invaders Beta")
+titleIcon = pygame.image.load('titleIcon.png')
+pygame.display.set_icon(titleIcon)
+
+# setting up the gamescreen
+height = 1000
+width = 500
+gameScreen = pygame.display.set_mode((height, width))
+
+# player
+
+playerImage = pygame.image.load('spaceship.png')
+playerPosX = 450
+playerChangeInX = 0
+playerPosY = 400
+
+# enemy
+
+enemyImage = pygame.image.load('aliens.png')
+enemyPosX = 450
+enemyChangeInX = 0.3
+enemyPosY = 100
+
+
+def player(x, y):
+    gameScreen.blit(playerImage, (x, y))
+
+
+def enemy(x, y):
+    gameScreen.blit(enemyImage, (x, y))
+
+
+def playerBoundaryChecker(x):
+    if x < 0:
+        x = 0
+    elif x > 935:
+        x = 935
+    return x
+
+def enemyBoundaryChecker(x):
+    y = 0
+    if x <= 0:
+        y = 0.3
+    elif x >= 935:
+        y = -0.3
+    return y
+
+
+# game loop
+# keeps the pygame window from closing, until the player closes it themself
+isGameRunning = True
+
+while isGameRunning:
+    gameScreen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            isGameRunning = False
+
+
+        #player movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerChangeInX = -0.1
+
+            if event.key == pygame.K_RIGHT:
+                playerChangeInX = 0.1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerChangeInX = 0.0
+
+
+
+
+    playerPosX += playerChangeInX
+    playerPosX = playerBoundaryChecker(playerPosX)
+    player(playerPosX, playerPosY)
+
+    #enemyPosX += enemyChangeInX
+    #enemyChangeInX = enemyBoundaryChecker(enemyPosX)
+
+    enemyPosX += enemyChangeInX
+    if enemyPosX >= 935:
+        enemyChangeInX = -0.3
+    elif enemyPosX <= 0:
+        enemyChangeInX = 0.3
+
+    enemy(enemyPosX, enemyPosY)
+
+    pygame.display.update()
